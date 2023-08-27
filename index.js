b4c5ce0c56963fbbb30d5a9f6e0bd58f950c46f9
ThilakSamplegithub <112916543+ThilakSamplegithub@users.noreply.github.com>
2023-08-28 03:39:26 +0530

    cors added

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express=require("express")
 const {connection}=require("./config/db")
+const cors=require("cors")
 require('dotenv').config()
 const {passengerRouter}=require("./Routes/passenger.routes")
 const cors=require("cors")
 const { driverRouter } = require("./Routes/driver.routes")
 const app=express()
 app.use(express.json())
+app.use(cors())
 app.get("/",(req,res)=>{
     res.setHeader("Content-Type","text/html")
     res.send(`<h1>Welcome</h1>`)
